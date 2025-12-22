b1a2ad7c187c3ed16f9053274bb07d3a859b4e36
Aritra <aritra.sadhukhan@codeclouds.co.in>
2025-12-22 18:19:44 +0530

    dev: temp

--- porichoyctl/cmd/init.go
+++ porichoyctl/cmd/init.go
@@ -1,20 +1,40 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
 
-type AdminInfo struct {
-	email           string
-	name            string
-	password        string
-	confirmPassword string
-}
+	"github.com/spf13/cobra"
+)
 
+// initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "Initialize Porichoy",
-	Long:  `Initialize Porichoy`,
-	Run: func(cmd *cobra.Command, args []string) {
-		adminInfo := AdminInfo{}
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
 
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("init called")
 	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
